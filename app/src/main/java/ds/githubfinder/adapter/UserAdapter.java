f73507a4d4c1a8c3adb34d5fb6ce6b4299f800b5
dionsaputra <dionsaputra666@gmail.com>
2019-01-15 18:09:33 +0700

    Add user layout

--- app/src/main/java/ds/githubfinder/adapter/UserAdapter.java
+++ app/src/main/java/ds/githubfinder/adapter/UserAdapter.java
@@ -8,12 +8,13 @@ import android.view.ViewGroup;
 import java.util.ArrayList;
 import java.util.List;
 
+import ds.githubfinder.R;
 import ds.githubfinder.models.User;
 
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
 
-    private List<User> dataList = new ArrayList<>();
+    private List<User> userList = new ArrayList<>();
 
     @Override
     public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -25,8 +26,13 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
 
     }
 
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public void addItems(List<User> dataList) {
-        this.dataList.addAll(dataList);
+        this.userList.addAll(dataList);
         notifyDataSetChanged();
     }
 
